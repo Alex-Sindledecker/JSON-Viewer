c971c256e1acf2f7cbd22c8889889cccabb94886
Alex Sindledecker <alexsindledecker1@gmail.com>
2023-09-21 23:02:05 -0700

    Setup backend EJS server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render(__dirname + "/views/index.ejs");
+});
+
+app.listen(port, () => {
+    console.log("App running on port 3000");
+});
